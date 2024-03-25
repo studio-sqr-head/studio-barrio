2a8d7c00da3ab57b53abc2432b4a9897badc6d83
James Rowbotham <james@altis.io>
2024-03-25 12:37:05 +0100

    feat: meta data

---
+++ app/manifest.ts
@@ -0,0 +1,55 @@
+import { MetadataRoute } from "next";
+
+import type { MetadataI } from "@/sanity/schemas";
+import { metadataQuery } from "@/sanity/lib/queries";
+import { sanityFetch } from "@/sanity/lib/fetch";
+
+export default async function manifest(): Promise<MetadataRoute.Manifest> {
+  const [metadata] = await Promise.all([
+    sanityFetch<MetadataI>({ query: metadataQuery }),
+  ]);
+
+  const { description, title } = metadata ?? {};
+
+  return {
+    name: title ?? "Studio Barrio",
+    short_name: title ?? "Studio Barrio",
+    description:
+      description ?? "Studio Barrio is a design UX studio based in Amsterdam.",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#FF7C4D",
+    icons: [
+      {
+        src: "/icon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+      {
+        src: "/icon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        src: "/icon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}

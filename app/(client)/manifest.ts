d60f0702cda94d7554354afde8c3144c2f20aaae
James Rowbotham <james@altis.io>
2024-03-25 12:31:40 +0100

    feat: add favicons

--- app/(client)/manifest.ts
+++ app/(client)/manifest.ts
@@ -16,39 +16,9 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
     short_name: title ?? "Studio Barrio",
     description:
       description ?? "Studio Barrio is a design UX studio based in Amsterdam.",
-    start_url: "/",
     display: "standalone",
     background_color: "#fff",
     theme_color: "#fff",
-    icons: [
-      {
-        src: "/favicon",
-        type: "image/png",
-      },
-      {
-        src: "/favicon-16x16.png",
-        sizes: "16x16",
-        type: "image/png",
-      },
-      {
-        src: "/favicon-32x32.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-      {
-        src: "/apple-touch-icon.png",
-        type: "image/png",
-      },
-    ],
+    start_url: "/",
   };
 }
